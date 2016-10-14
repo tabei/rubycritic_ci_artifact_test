d6bf4f95ef068f57d5426e6977e3054e7db8e12a
tabei_k <katsuhiko.tabei@andrometec.com>
2016-10-14 10:14:10 +0900

    test

---
+++ lib/rubycritic_ci_test.rb
@@ -0,0 +1,16 @@
+require 'rubycritic_ci_test/version'
+
+module RubycriticCiTest
+  class FizzBuzz
+    def fizzbuzz(limit)
+      (0..limit).each_with_object([]) do |e, memo|
+        memo << case
+        when e % 15 == 0 then "FizzBuzz"
+        when e % 5 == 0 then "Buzz"
+        when e % 3 == 0 then "Fizz"
+        else e.to_s
+        end
+      end
+    end
+  end
+end
---
+++ lib/rubycritic_ci_test/version.rb
@@ -0,0 +1,3 @@
+module RubycriticCiTest
+  VERSION = '0.1.0'
+end
